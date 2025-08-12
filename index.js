20ac346cec23380e7e8bf387f4dd1f4f5e18f58a
cristian <cristiancocabjn@gmail.com>
2025-08-11 22:26:59 -0400

    exercise: 3.5 step: 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -56,6 +58,34 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    let id
+    do{
+        id = Math.floor(Math.random() * 1000000) 
+    } while(persons.find(person => person.id === id))
+    return id
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if((!body.name || !body.number)) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number 
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
