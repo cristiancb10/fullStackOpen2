3e60e846471cc1ded921b87877025f21a1ac54ca
cristian <cristiancocabjn@gmail.com>
2025-08-29 00:54:30 -0400

    exercise: 3.16 step: 4

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+    
+    next(error)
+}
+
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -35,15 +45,17 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 
-    Person.findById(request.params.id).then(person => {
-        if(person) {
-            response.json(person)
-        }else {
-            response.status(404).end()
-        }
-    })
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -83,6 +95,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
